227338433f7773d6fb3821425b25e851eb1dff6e
izharude <duntuldentel@gmail.com>
2026-07-03 18:19:58 +0800

    Add files via upload

--- UI/WhipSelectionUI.cs
+++ UI/WhipSelectionUI.cs
@@ -8,7 +8,8 @@ using Terraria.UI;
 using Terraria.GameContent;
 using Terraria.GameContent.UI.Elements;
 using Terraria.Audio;
-using TheSanity.Rarities; 
+using TheSanity.Rarities;
+using TheSanity.Items.Whips;
 
 namespace TheSanity
 {
@@ -20,49 +21,54 @@ namespace TheSanity
         public override void OnInitialize() {
             containerPanel = new UIPanel();
             containerPanel.SetPadding(10);
-            containerPanel.Left.Set(-245f, 0.5f); 
-            containerPanel.Top.Set(-60f, 0.5f);   
-            containerPanel.Width.Set(490f, 0f);   
+            containerPanel.Left.Set(-295f, 0.5f);
+            containerPanel.Top.Set(-60f, 0.5f);
+            containerPanel.Width.Set(590f, 0f);
             containerPanel.Height.Set(115f, 0f);
-            containerPanel.BackgroundColor = new Color(23, 23, 33, 240); 
+            containerPanel.BackgroundColor = new Color(23, 23, 33, 240);
 
-            // SOLUSI: Gunakan warna teal default saat loading awal agar tidak NullReferenceException
             Color fallbackColor = new Color(60, 255, 200);
-            containerPanel.BorderColor = fallbackColor; 
+            containerPanel.BorderColor = fallbackColor;
 
             titleText = new UIText("Select Whip Tag Sub-Power", 0.6f, true);
-            titleText.HAlign = 0.5f; 
-            titleText.Top.Set(10f, 0f); 
+            titleText.HAlign = 0.5f;
+            titleText.Top.Set(10f, 0f);
             titleText.TextColor = fallbackColor;
             containerPanel.Append(titleText);
 
-            int[] allVanillaWhips = {
-                ItemID.BlandWhip,   
-                ItemID.ThornWhip,   
-                ItemID.BoneWhip,    
-                ItemID.FireWhip,    
-                ItemID.CoolWhip,    
-                ItemID.SwordWhip,   
-                ItemID.ScytheWhip,  
-                ItemID.MaceWhip,    
-                ItemID.RainbowWhip  
+            Func<int>[] allWhips = new Func<int>[] {
+                () => ItemID.BlandWhip,
+                () => ItemID.ThornWhip,
+                () => ModContent.ItemType<FeatherWireWhip>(),
+                () => ItemID.BoneWhip,
+                () => ItemID.FireWhip,
+                () => ModContent.ItemType<AbyssalKrakenTentacle>(),
+                () => ItemID.CoolWhip,
+                () => ItemID.SwordWhip,
+                () => ItemID.ScytheWhip,
+                () => ItemID.MaceWhip,
+                () => ItemID.RainbowWhip
             };
 
-            float startX = 13f;   
-            float spacingX = 50f; 
+            float buttonWidth = 44f;
+            float spacingX = 50f;
+            float totalRowWidth = ((allWhips.Length - 1) * spacingX) + buttonWidth;
+            float startX = (570f - totalRowWidth) / 2f;
 
-            for (int i = 0; i < allVanillaWhips.Length; i++) {
-                int whipItemID = allVanillaWhips[i];
-                WhipSlotButton button = new WhipSlotButton(whipItemID);
+            for (int i = 0; i < allWhips.Length; i++) {
+                WhipSlotButton button = new WhipSlotButton(allWhips[i]);
                 button.Left.Set(startX + (i * spacingX), 0f);
-                button.Top.Set(45f, 0f); 
-                button.Width.Set(44f, 0f);
+                button.Top.Set(45f, 0f);
+                button.Width.Set(buttonWidth, 0f);
                 button.Height.Set(44f, 0f);
                 
                 button.OnLeftClick += (evt, element) => {
-                    Main.LocalPlayer.GetModPlayer<BlackwhipPlayer>().selectedWhipTagType = whipItemID;
-                    SoundEngine.PlaySound(SoundID.MenuTick); 
-                    ModContent.GetInstance<BlackwhipSystem>().CloseWhipUI(); 
+                    WhipSlotButton clickedButton = (WhipSlotButton)element;
+                    if (clickedButton.TargetWhipID > 0) {
+                        Main.LocalPlayer.GetModPlayer<BlackwhipPlayer>().selectedWhipTagType = clickedButton.TargetWhipID;
+                        SoundEngine.PlaySound(SoundID.MenuTick);
+                        ModContent.GetInstance<BlackwhipSystem>().CloseWhipUI();
+                    }
                 };
 
                 containerPanel.Append(button);
@@ -72,9 +78,7 @@ namespace TheSanity
 
         public override void Update(GameTime gameTime) {
             base.Update(gameTime);
-
             if (containerPanel != null && titleText != null) {
-                // Ambil rarity dengan aman menggunakan null-check
                 var rarity = ModContent.GetInstance<BlackwhipRarity>();
                 if (rarity != null) {
                     Color currentRarityColor = rarity.RarityColor;
@@ -87,11 +91,12 @@ namespace TheSanity
 
     public class WhipSlotButton : UIElement
     {
-        public int TargetWhipID;
+        private readonly Func<int> _getItemID;
+        public int TargetWhipID => _getItemID();
         public bool Hovered;
 
-        public WhipSlotButton(int whipItemID) {
-            TargetWhipID = whipItemID;
+        public WhipSlotButton(Func<int> getItemID) {
+            _getItemID = getItemID;
         }
 
         public override void MouseOver(UIMouseEvent evt) {
@@ -106,21 +111,22 @@ namespace TheSanity
         }
 
         protected override void DrawSelf(SpriteBatch spriteBatch) {
+            int currentID = TargetWhipID;
+            if (currentID <= 0) return;
+
             CalculatedStyle style = GetInnerDimensions();
-            
             Texture2D slotBackTex = TextureAssets.InventoryBack.Value;
             Color boxColor = Hovered ? Color.LightGreen : Color.White * 0.8f;
             
-            if (Main.LocalPlayer.GetModPlayer<BlackwhipPlayer>().selectedWhipTagType == TargetWhipID) {
-                // Ambil rarity secara aman di dalam proses draw game
+            if (Main.LocalPlayer.GetModPlayer<BlackwhipPlayer>().selectedWhipTagType == currentID) {
                 var rarity = ModContent.GetInstance<BlackwhipRarity>();
-                boxColor = rarity != null ? rarity.RarityColor : new Color(60, 255, 200); 
+                boxColor = rarity != null ? rarity.RarityColor : new Color(60, 255, 200);
             }
 
             spriteBatch.Draw(slotBackTex, style.Position(), null, boxColor, 0f, Vector2.Zero, style.Width / slotBackTex.Width, SpriteEffects.None, 0f);
-
-            Main.instance.LoadItem(TargetWhipID);
-            Texture2D whipItemTex = TextureAssets.Item[TargetWhipID].Value;
+            
+            Main.instance.LoadItem(currentID);
+            Texture2D whipItemTex = TextureAssets.Item[currentID].Value;
             
             float itemScale = 1f;
             if (whipItemTex.Width > style.Width || whipItemTex.Height > style.Height) {
@@ -129,12 +135,11 @@ namespace TheSanity
 
             Vector2 innerCenter = new Vector2(style.Width, style.Height) / 2f;
             Vector2 drawPosition = style.Position() + innerCenter;
-
             spriteBatch.Draw(whipItemTex, drawPosition, null, Color.White, 0f, whipItemTex.Size() / 2f, itemScale, SpriteEffects.None, 0f);
 
             if (Hovered) {
                 Item hoverDummy = new Item();
-                hoverDummy.SetDefaults(TargetWhipID);
+                hoverDummy.SetDefaults(currentID);
                 Main.hoverItemName = hoverDummy.Name;
             }
         }
